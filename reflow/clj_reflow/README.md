963a77384af80f3be369d67f620d62b4d5e142d5
James <james.kominick@gmail.com>
2016-08-19 23:40:02 -0400

    reflow

--- reflow/clj_reflow/README.md
+++ reflow/clj_reflow/README.md
@@ -1,3 +1,12 @@
 # reflow
 
 [text-reflow](https://www.reddit.com/r/dailyprogrammer/comments/4ybbcz/20160818_challenge_279_intermediate_text_reflow/)
+
+`lein run` to run off challenge input in `resources/input.txt`
+
+or in the repl:
+```
+(require 'reflow.core)
+(trim content-string max-line-length)
+(justify content-string max-line-length)
+```
--- reflow/clj_reflow/src/reflow/core.clj
+++ reflow/clj_reflow/src/reflow/core.clj
@@ -8,13 +8,20 @@
 (defn lines->words [lines]
   (mapcat #(clojure.string/split % #" ") lines))
 
-(defn sized-words [words]
+(defn sized-words
+  "map words into vectors containing
+  [word-length word]"
+  [words]
   (map (fn [word] [(count word) word]) words))
 
-(defn limiter [maxlen acc [size word]]
-  (let [line (first acc)
-        line-size (first line)
-        content (last line)
+(defn limiter
+  "maxlen to limit to
+  acc is a list of completed lines, first is the current
+  each line is a vector containin [size-of-line-int list-of-words]"
+  [maxlen acc [size word]]
+  (let [line (first acc)       ; current line
+        line-size (first line) ; size of current line
+        content (last line)    ; content of current line
         new-line-size (+ line-size size)]
     (cond
       (zero? size) (concat [[0 '()] [size '()]] acc)
@@ -24,7 +31,9 @@
                          (rest acc))
       :else (cons [size (list word)] acc))))
 
-(defn limit [content maxlen]
+(defn limit
+  "Reflow text content to keep lines within maxlen."
+  [content maxlen]
   (->> (lines->words content)
        (sized-words)
        (reduce (partial limiter maxlen) (list [0 '()]))
@@ -34,39 +43,55 @@
        (map #(clojure.string/join " " %))
        (clojure.string/join "\n")))
 
-(defn padder [maxlen line len spaces])
+(defn space-indices
+  "Generate an infinite seq of space
+  indices cycling from the outside in."
+  [num-words]
+  (->> (filter odd? (range num-words))
+       (#(split-at (/ (count %) 2) %))
+       ((fn [[top bot]]
+            (cond
+              (empty? top) top
+              (empty? bot) top
+              :else (interleave
+                      (cycle top)
+                      (cycle (reverse bot))))))))
 
 (defn pad-line
+  "Increase space sizes from the outside in
+  to padd the line to the maxlen size."
   [maxlen line]
   (let [line (->> (clojure.string/split line #" ")
                   (interpose " ")
                   (into []))
         len (apply + (map count line))
         num-words (count line)
-        spaces (->> (filter odd? (range num-words))
-                    (split-at (/ num-words 2))
-                    ((fn [inds] (interleave (first inds) (reverse (last inds))))))]
-    ;(reduce (partial padder maxlen) )
+        spaces (space-indices num-words)]
     (loop [maxlen maxlen
            line line
            len len
            spaces spaces]
      (let [space (first spaces)
            newspaces (rest spaces)]
-       (println (str "len: " len ", spaces: " spaces))
-       (if (or (>= len maxlen) (empty? spaces))
-         line
-         (recur maxlen (update line space #(str % " ")) (inc len) newspaces))))))
+       (if (and (< len maxlen) (not (nil? space)))
+         (recur maxlen (update line space #(str % " ")) (inc len) newspaces)
+         line)))))
 
-(defn justify [content maxlen]
+(defn justify
+  "Reflow text to maxlen limit and
+  pad with spaces from the outside
+  in to fit to the maxlen size."
+  [content maxlen]
   (as-> (limit content maxlen) _
       (clojure.string/split _ #"\n")
-      (map #(pad-line maxlen %) _)))
+      (map #(pad-line maxlen %) _)
+      (map clojure.string/join _)
+      (clojure.string/join "\n" _)))
 
 (defn -main [& args]
   (let [content (read-file "input.txt")]
     (println "Text-reflowing...")
     (println (limit content 40))
-    (println "Justifying...")
+    (println "\n\nJustifying...")
     (println (justify content 40))))
 
