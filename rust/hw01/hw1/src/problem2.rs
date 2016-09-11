1c58b6fcbd0ec23832bed697396daae7dfb3ae46
James <james.kominick@gmail.com>
2016-09-11 19:41:09 -0400

    rust hw | prob.3
    hanoi

--- rust/hw01/hw1/src/problem2.rs
+++ rust/hw01/hw1/src/problem2.rs
@@ -1,9 +1,9 @@
 pub fn sieve(n: u32) -> Vec<u32> {
-    let mut acc = (2..n+1).collect::<Vec<_>>();
+    let acc = (2..n+1).collect::<Vec<_>>();
     (2..n+1).fold(acc, |mut acc, item| {
         match acc.binary_search(&item) {
             Err(_) => acc,
-            Ok(i) => {
+            Ok(_) => {
                 let mut np = item + item;
                 while np <= n {
                     match acc.binary_search(&np) {
