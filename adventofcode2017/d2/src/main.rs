acff1e843c0dff5cc1ec2f2658d460156cb5dbe8
James Kominick <james.kominick@gmail.com>
2017-12-05 20:24:23 -0500

    cleanup

--- adventofcode2017/d2/src/main.rs
+++ adventofcode2017/d2/src/main.rs
@@ -5,7 +5,8 @@ static INPUT: &'static str = include_str!("../input.txt");
 
 type Result<T> = std::result::Result<T, Box<std::error::Error>>;
 
-fn input() -> Result<Vec<Vec<usize>>> {
+
+fn read_sheet() -> Result<Vec<Vec<usize>>> {
     let mut sheet = vec![];
     for line in INPUT.trim().lines() {
         let mut row = vec![];
@@ -18,44 +19,50 @@ fn input() -> Result<Vec<Vec<usize>>> {
     Ok(sheet)
 }
 
-fn checksum(elems: &[usize]) -> usize {
-    let mut min = usize::max_value();
-    let mut max = usize::min_value();
-    for e in elems {
-        if *e < min { min = *e; }
-        if *e > max { max = *e; }
+
+mod p1 {
+    pub fn checksum_line(elems: &[usize]) -> usize {
+        let mut min = usize::max_value();
+        let mut max = usize::min_value();
+        for e in elems {
+            if *e < min { min = *e; }
+            if *e > max { max = *e; }
+        }
+        max - min
     }
-    max - min
+
+    pub fn checksum(sheet: &[Vec<usize>]) -> usize {
+        sheet.iter().fold(0, |acc, row| acc + checksum_line(row.as_slice()))
+    }
 }
 
-fn checksum_sheet(sheet: &[Vec<usize>]) -> usize {
-    sheet.iter().fold(0, |acc, row| acc + checksum(row.as_slice()))
-}
 
-
-fn checkdiv(elems: &[usize]) -> usize {
-    for e in elems {
-        for e2 in elems {
-            if e != e2 && e % e2 == 0 {
-                if e > e2 { return e / e2 }
-                else { return e2 / e }
+mod p2 {
+    pub fn checksum_line(elems: &[usize]) -> usize {
+        for e in elems {
+            for e2 in elems {
+                if e != e2 && e % e2 == 0 {
+                    if e > e2 { return e / e2 }
+                    else { return e2 / e }
+                }
             }
         }
+        unreachable!();
     }
-    unreachable!();
-}
 
-fn checksumdiv_sheet(sheet: &[Vec<usize>]) -> usize {
-    sheet.iter().fold(0, |acc, row| acc + checkdiv(row.as_slice()))
+    pub fn checksum(sheet: &[Vec<usize>]) -> usize {
+        sheet.iter().fold(0, |acc, row| acc + checksum_line(row.as_slice()))
+    }
 }
 
 
 fn run() -> Result<()> {
-    let input = input()?;
-    println!("d2-p1: {}", checksum_sheet(input.as_slice()));
-    println!("d2-p2: {}", checksumdiv_sheet(input.as_slice()));
+    let sheet = read_sheet()?;
+    println!("d2-p1: {}", p1::checksum(sheet.as_slice()));
+    println!("d2-p2: {}", p2::checksum(sheet.as_slice()));
     Ok(())
 }
+
 
 fn main() {
     if let Err(e) = run() {
@@ -71,7 +78,7 @@ mod tests {
     fn part1() {
         [(vec![5, 1, 9, 5], 8), (vec![7, 5, 3], 4), (vec![2, 4, 6, 8], 6)].iter()
             .for_each(|&(ref row, sum)| {
-                assert_eq!(checksum(&row), sum, "row: {:?}, expected sum: {}", row, sum);
+                assert_eq!(p1::checksum_line(&row), sum, "row: {:?}, expected sum: {}", row, sum);
             });
     }
 
@@ -82,14 +89,14 @@ mod tests {
             vec![7, 5, 3],
             vec![2, 4, 6, 8],
         ];
-        assert_eq!(checksum_sheet(&input), 18);
+        assert_eq!(p1::checksum(&input), 18);
     }
 
     #[test]
     fn part2() {
         [(vec![5, 9, 2, 8], 4), (vec![9, 4, 7, 3], 3), (vec![3, 8, 6, 5], 2)].iter()
             .for_each(|&(ref row, div)| {
-                assert_eq!(checkdiv(&row), div, "row: {:?}, expected div: {}", row, div);
+                assert_eq!(p2::checksum_line(&row), div, "row: {:?}, expected div: {}", row, div);
             });
     }
 
@@ -100,6 +107,6 @@ mod tests {
             vec![9, 4, 7, 3],
             vec![3, 8, 6, 5],
         ];
-        assert_eq!(checksumdiv_sheet(&input), 9);
+        assert_eq!(p2::checksum(&input), 9);
     }
 }
