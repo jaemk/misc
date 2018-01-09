3d7362626ca4e0f6b912d961fc4935991e1a0680
James Kominick <james.kominick@gmail.com>
2018-01-09 00:23:44 -0500

    d1-c
    - update with utils & example tests

--- adventofcode2017/d01/c/src/main.c
+++ adventofcode2017/d01/c/src/main.c
@@ -2,66 +2,21 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include "../../../cutils/utils.h"
 
 
-char* read_input(const char* path) {
-    FILE* f = fopen(path, "r");
-    if (f == NULL) {
-        perror("Error opening file");
-        return NULL;
-    }
-    fseek(f, 0, SEEK_END);
-    size_t len = ftell(f);
-    rewind(f);
-    char* content = malloc((len + 1) * sizeof(char));
-    fread(content, sizeof(char), len, f);
-    fclose(f);
-    return content;
-}
+size_t part1(char* data) {
+    String string_ = string_from_cstr(data);
+    Str str = string_trim_whitespace(&string_);
+    size_t len = str.len;
 
-
-typedef struct {
-    size_t start, end;
-    char* data;
-} Trim;
-
-Trim* trim_new(char* str) {
-    size_t start = 0;
-    while (isspace(str[start]))
-        start += 1;
-
-    size_t end = strlen(str) - 1;
-    while (isspace(str[end]))
-        end -= 1;
-
-    Trim* tr = malloc(sizeof(Trim));
-    if (tr == NULL)
-        return NULL;
-
-    tr->start = start;
-    tr->end = end;
-    tr->data = str;
-    return tr;
-}
-
-size_t trim_size(Trim* tr) {
-    return 1 + tr->end - tr->start;
-}
-
-char trim_get(Trim* tr, size_t i) {
-    return tr->data[i];
-}
-
-
-size_t part1(Trim* input) {
-    size_t len = trim_size(input);
     size_t sum = 0;
-    if (trim_get(input, 0) == trim_get(input, len-1))
-        sum += trim_get(input, 0) - '0';
+    if (str_index(&str, 0) == str_index(&str, len-1))
+        sum += str_index(&str, 0) - '0';
 
     for (size_t i = 0; i < len - 1; i++) {
-        char a = trim_get(input, i);
-        if (a == trim_get(input, i+1))
+        char a = str_index(&str, i);
+        if (a == str_index(&str, i+1))
             sum += a - '0';
     }
     return sum;
@@ -70,39 +25,74 @@ size_t part1(Trim* input) {
 
 size_t step_index(size_t curr, size_t step, size_t limit) {
     size_t next = curr + step;
-    while (next > limit)
+    while (next >= limit)
         next -= limit;
     return next;
 }
 
 
-size_t part2(Trim* input) {
-    size_t len = trim_size(input);
+size_t part2(char* data) {
+    String s = string_from_cstr(data);
+    Str str = string_trim_whitespace(&s);
+    size_t len = str.len;
     size_t half = len / 2;
     size_t sum = 0;
     for (size_t i = 0; i < len; i++) {
         size_t next = step_index(i, half, len);
-        char a = trim_get(input, i);
-        if (a == trim_get(input, next))
+        char a = str_index(&str, i);
+        if (a == str_index(&str, next))
             sum += a - '0';
     }
     return sum;
 }
 
 
+void test_examples() {
+    printf("|---- Part1... ");
+    char* p1_cases[] = {"1122", "1111", "1234", "91212129"};
+    size_t p1_expected[] = {3, 4, 0, 9};
+    size_t p1_len = sizeof(p1_cases) / sizeof(p1_cases[0]);
+    for (size_t i = 0; i < p1_len; i++) {
+        size_t ans = part1(p1_cases[i]);
+        if (ans != p1_expected[i]) {
+            fprintf(stderr, "Part1: Input: %s, Expected %lu, got %lu\n",
+                    p1_cases[i], p1_expected[i], ans);
+            abort();
+        }
+    }
+    printf("OK\n");
+
+    printf("|---- Part2... ");
+    char* p2_cases[] = {"1212", "1221", "123425", "123123", "12131415"};
+    size_t p2_expected[] = {6, 0, 4, 12, 4};
+    size_t p2_len = sizeof(p2_cases) / sizeof(p2_cases[0]);
+    for (size_t i = 0; i < p2_len; i++) {
+        size_t ans = part2(p2_cases[i]);
+        if (ans != p2_expected[i]) {
+            fprintf(stderr, "Part2: Input: %s, Expected %lu, got %lu\n",
+                    p2_cases[i], p2_expected[i], ans);
+            abort();
+        }
+    }
+    printf("OK\n");
+}
+
+
 int main() {
-    char* data = read_input("../input.txt");
+    printf("\n");
+    printf("Running examples...\n");
+    test_examples();
+    printf("\n");
+
+    printf("Running main input...\n");
+    char* data = read_file("../input.txt");
     if (data == NULL)
-        return -1;
+        return 1;
+    printf("day1-p1: %lu\n", part1(data));
+    printf("day1-p2: %lu\n", part2(data));
+    printf("\n");
 
-    Trim* input = trim_new(data);
-    if (input == NULL)
-        return -1;
-
-    printf("day1-p1: %lu\n", part1(input));
-    printf("day1-p2: %lu\n", part2(input));
-
-    free(input);
+    free(data);
     return 0;
 }
 
