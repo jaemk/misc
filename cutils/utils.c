85202483921a2ee4beeddcffc083e476132d6769
James Kominick <james.kominick@gmail.com>
2018-01-11 23:01:41 -0500

    update cutils
    - renamed private struct fields
    - add functions for accessing len/capacity
    - add more vec functions

--- cutils/utils.c
+++ cutils/utils.c
@@ -5,11 +5,23 @@
 #include "utils.h"
 
 
+/* Increase capacity by a factor of 2, unless it's already really big,
+ * then increase by a constant value
+ */
+size_t __inc_cap(size_t current) {
+    if (current > 8192) {
+        return current + 8192;
+    } else {
+        return current * 2;
+    }
+}
+
+
 /* ----------- String ------------- */
 
 
 String string_new() {
-    String s = { .__data=NULL, .len=0, .cap=0 };
+    String s = { .__data=NULL, .__len=0, .__cap=0 };
     return s;
 }
 
@@ -20,11 +32,11 @@ String string_with_capactiy(size_t cap) {
         abort();
     }
     memset(data, '\0', cap + 1);
-    String s = { .__data=data, .len=0, .cap=cap };
+    String s = { .__data=data, .__len=0, .__cap=cap };
     return s;
 }
 
-String string_copy_from_cstr(char* cstr) {
+String string_copy_from_cstr(const char* cstr) {
     size_t len = strlen(cstr);
     char* data = malloc((len + 1) * sizeof(char));
     if (data == NULL) {
@@ -33,88 +45,88 @@ String string_copy_from_cstr(char* cstr) {
     }
     memset(data, '\0', len + 1);
     strncpy(data, cstr, len);
-    String s = { .__data=data, .len=len, .cap=len };
+    String s = { .__data=data, .__len=len, .__cap=len };
     return s;
+}
+
+size_t string_len(String* s) {
+    return s->__len;
+}
+
+size_t string_cap(String* s) {
+    return s->__cap;
 }
 
 void string_resize(String* s, size_t new_cap) {
     if (new_cap == 0)
         new_cap = 16;
 
-    size_t n_trailing = (new_cap + 1) - s->len;
+    size_t n_trailing = (new_cap + 1) - s->__len;
     char* data = realloc(s->__data, (new_cap + 1) * sizeof(char));
     if (data == NULL) {
         fprintf(stderr, "String resize failure\n");
         abort();
     }
-    memset(data + s->len, '\0', n_trailing);
+    memset(data + s->__len, '\0', n_trailing);
     s->__data = data;
-    s->cap = new_cap;
-}
-
-size_t __inc_cap(size_t current) {
-    if (current > 8192) {
-        return current + 8192;
-    } else {
-        return current * 2;
-    }
+    s->__cap = new_cap;
 }
 
 void string_push_char(String* s, char c) {
-    size_t avail = s->cap - s->len;
+    size_t avail = s->__cap - s->__len;
     if (avail == 0) {
-        size_t new_cap = __inc_cap(s->cap);
+        size_t new_cap = __inc_cap(s->__cap);
         string_resize(s, new_cap);
     }
-    s->__data[s->len] = c;
-    s->len += 1;
+    s->__data[s->__len] = c;
+    s->__len += 1;
 }
 
 void string_push_str(String* s, Str* str) {
-    string_push_cstr_bound(s, str->__data+str->__start, str->len);
+    string_push_cstr_bound(s, str->__data, str->__len);
 }
 
-void string_push_cstr(String* s, char* cstr) {
+void string_push_cstr(String* s, const char* cstr) {
     size_t len = strlen(cstr);
     string_push_cstr_bound(s, cstr, len);
 }
 
-void string_push_cstr_bound(String* s, char* cstr, size_t str_len) {
-    size_t avail = s->cap - s->len;
+void string_push_cstr_bound(String* s, const char* cstr, size_t str_len) {
+    size_t avail = s->__cap - s->__len;
     if (str_len > avail) {
-        size_t new_cap = __inc_cap(s->cap);
-        if (str_len > (new_cap - s->len)) {
-            new_cap += str_len - (new_cap - s->len);
+        size_t new_cap = __inc_cap(s->__cap);
+        if (str_len > (new_cap - s->__len)) {
+            new_cap += str_len - (new_cap - s->__len);
         }
         string_resize(s, new_cap);
     }
     size_t count = 0;
     while (*cstr && count < str_len) {
-        s->__data[s->len] = *cstr;
-        s->len++;
+        s->__data[s->__len] = *cstr;
+        s->__len++;
         count++;
         cstr++;
     }
 }
 
 char string_index(String* s, size_t index) {
-    if (index >= s->len) {
-        fprintf(stderr, "Out of bounds: strlen: %lu, index: %lu", s->len, index);
+    if (index >= s->__len) {
+        fprintf(stderr, "Out of bounds: strlen: %lu, index: %lu", s->__len, index);
         abort();
     }
     return s->__data[index];
 }
 
 char* string_index_ref(String* s, size_t index) {
-    if (index >= s->len) {
-        fprintf(stderr, "Out of bounds: strlen: %lu, index: %lu", s->len, index);
+    if (index >= s->__len) {
+        fprintf(stderr, "Out of bounds: strlen: %lu, index: %lu", s->__len, index);
         abort();
     }
     return s->__data + index;
 }
 
 Str string_as_str(String* s) {
-    Str str = { .__data=s->__data, .__start=0, .len=s->len };
+    Str str = { .__data=s->__data, .__len=s->__len };
     return str;
 }
 
@@ -123,29 +135,55 @@ Str string_trim_whitespace(String* s) {
     return str_trim_whitespace(&str);
 }
 
+Vec string_split_whitespace(String* s) {
+    Str str = string_as_str(s);
+    return str_split_whitespace(&str);
+}
+
+Vec string_split_lines(String* s) {
+    Str str = string_as_str(s);
+    return str_split_lines(&str);
+}
+
 char* string_as_cstr(String* s) {
     return s->__data;
 }
 
+void string_clear(String* s) {
+    if (s->__data == NULL)
+        return;
+    memset(s->__data, '\0', s->__len);
+    s->__len = 0;
+}
+
 void string_drop_inner(void* string_ptr) {
     String* s = (String*)string_ptr;
-    if (s->__data != NULL)
-        free(s->__data);
+    if (s->__data == NULL)
+        return;
+    free(s->__data);
+    s->__len = 0;
+    s->__cap = 0;
 }
 
 
 /* ----------- Str -------------- */
 
 
-Str str_from_cstr(char* cstr) {
+Str str_from_cstr(const char* cstr) {
     size_t len = strlen(cstr);
-    Str s = { .__data=cstr, .__start=0, .len=len };
+    Str s = { .__data=cstr, .__len=len };
+    return s;
+}
+
+
+Str str_from_ptr_len(const char* ptr, size_t len) {
+    Str s = { .__data=ptr, .__len=len };
     return s;
 }
 
 Str str_trim_whitespace(Str* s) {
     size_t start = 0;
-    size_t end = s->len;
+    size_t end = s->__len;
     while (start < end && isspace(str_index(s, start)))
         start++;
 
@@ -160,35 +198,83 @@ Str str_trim_whitespace(Str* s) {
         len = end - start + 1;
     }
 
-    Str str = { .__data=s->__data, .__start=start, .len=len };
+    Str str = { .__data=(s->__data + start), .__len=len };
     return str;
 }
 
-/* void str_split_whitespace(Vec* v, Str* str) { */
-/*     char* ptr = str->__data + str->__start */
-/*     return vec_with_capacity(sizeof(Str*), str->len); */
-/* } */
+size_t str_len(Str* str) {
+    return str->__len;
+}
+
+Vec str_split_lines(Str* str) {
+    const char* ptr = str->__data;
+    size_t len = str->__len;
+    size_t start = 0;
+    size_t end = 0;
+    Vec v = vec_new(sizeof(Str));
+    while (start <= len) {
+        end = start;
+        while (end <= len && *(ptr + end) != '\n') {
+            end++;
+        }
+        if (end >= len) {
+            end = len;
+        }
+        Str str = str_from_ptr_len((ptr + start), (end - start));
+        vec_push(&v, &str);
+        start = end + 1;
+    }
+    return v;
+}
+
+Vec str_split_whitespace(Str* str) {
+    const char* ptr = str->__data;
+    size_t len = str->__len;
+    size_t start = 0;
+    size_t end = 0;
+    Vec v = vec_new(sizeof(Str));
+    while (start < len) {
+        while (start < len && isspace(*(ptr + start))) {
+            start++;
+        }
+        if (start >= len) {
+            break;
+        }
+        end = start;
+        while (end < len && !isspace(*(ptr + end))) {
+            end++;
+        }
+        Str str = str_from_ptr_len((ptr + start), (end - start));
+        vec_push(&v, &str);
+        start = end + 1;
+    }
+    return v;
+}
+
+const char* str_as_ptr(Str* str) {
+    return str->__data;
+}
 
 String str_to_owned_string(Str* str) {
-    String s = string_with_capactiy(str->len);
+    String s = string_with_capactiy(str->__len);
     string_push_str(&s, str);
     return s;
 }
 
 char str_index(Str* str, size_t ind) {
-    if (ind >= str->len) {
-        fprintf(stderr, "Out of bounds: strlen: %lu, index: %lu", str->len, ind);
+    if (ind >= str->__len) {
+        fprintf(stderr, "Out of bounds: strlen: %lu, index: %lu", str->__len, ind);
         abort();
     }
-    return str->__data[ind+str->__start];
+    return str->__data[ind];
 }
 
-char* str_index_ref(Str* str, size_t ind) {
-    if (ind >= str->len) {
-        fprintf(stderr, "Out of bounds: strlen: %lu, index: %lu", str->len, ind);
+const char* str_index_ref(Str* str, size_t ind) {
+    if (ind >= str->__len) {
+        fprintf(stderr, "Out of bounds: strlen: %lu, index: %lu", str->__len, ind);
         abort();
     }
-    return str->__data + ind + str->__start;
+    return str->__data + ind;
 }
 
 String read_file(const char* path) {
@@ -204,7 +290,7 @@ String read_file(const char* path) {
     fread(content, sizeof(char), len, f);
     content[len] = '\0';
     fclose(f);
-    String s = { .__data=content, .len=len, .cap=len };
+    String s = { .__data=content, .__len=len, .__cap=len };
     return s;
 }
 
@@ -213,7 +299,7 @@ String read_file(const char* path) {
 
 
 Vec vec_new(size_t item_size) {
-    Vec v = { .__data=NULL, .__item_size=item_size, .len=0, .cap=0 };
+    Vec v = { .__data=NULL, .__item_size=item_size, .__len=0, .__cap=0 };
     return v;
 }
 
@@ -223,8 +309,16 @@ Vec vec_with_capacity(size_t item_size, size_t cap) {
         fprintf(stderr, "Vec alloc failure\n");
         abort();
     }
-    Vec v = { .__data=data, .__item_size=item_size, .len=0, .cap=cap };
+    Vec v = { .__data=data, .__item_size=item_size, .__len=0, .__cap=cap };
     return v;
+}
+
+size_t vec_len(Vec* v) {
+    return v->__len;
+}
+
+size_t vec_cap(Vec* v) {
+    return v->__cap;
 }
 
 void vec_resize(Vec* v, size_t new_cap) {
@@ -237,42 +331,59 @@ void vec_resize(Vec* v, size_t new_cap) {
         abort();
     }
     v->__data = data;
-    v->cap = new_cap;
+    v->__cap = new_cap;
 }
 
 void vec_push(Vec* v, void* obj) {
-    size_t avail = v->cap - v->len;
+    size_t avail = v->__cap - v->__len;
     if (avail == 0) {
-        size_t new_cap = __inc_cap(v->cap);
+        size_t new_cap = __inc_cap(v->__cap);
         vec_resize(v, new_cap);
     }
-    char* offset = (char*)v->__data + (v->len * v->__item_size);
+    char* offset = (char*)v->__data + (v->__len * v->__item_size);
     memcpy((void*)offset, obj, v->__item_size);
-    v->len++;
+    v->__len++;
 }
 
 void* vec_index_ref(Vec* v, size_t ind) {
-    if (ind >= v->len) {
-        fprintf(stderr, "Out of bounds: veclen: %lu, index: %lu", v->len, ind);
+    if (ind >= v->__len) {
+        fprintf(stderr, "Out of bounds: veclen: %lu, index: %lu", v->__len, ind);
         abort();
     }
     char* offset = (char*)v->__data + (ind * v->__item_size);
     return (void*)offset;
 }
 
-void vec_drop_inner(void* vec_ptr) {
-    Vec* v = (Vec*)vec_ptr;
-    if (v->__data != NULL)
-        free(v->__data);
+void vec_iter_ref(Vec* v, mapFn func) {
+    size_t len = vec_len(v);
+    for (size_t i = 0; i < len; i++) {
+        void* ref = vec_index_ref(v, i);
+        func(ref);
+    }
 }
 
-void vec_drop_inner_each(Vec* v, dropFn drop) {
+void vec_clear(Vec* v, mapFn drop) {
     if (v->__data == NULL)
         return;
-    for (size_t i = 0; i < v->len; i++) {
-        char* offset = (char*)v->__data + (i * v->__item_size);
-        drop((void*)offset);
-    }
+    vec_iter_ref(v, drop);
+    v->__len = 0;
+}
+
+void vec_drop_inner(void* vec_ptr) {
+    Vec* v = (Vec*)vec_ptr;
+    if (v->__data == NULL)
+        return;
     free(v->__data);
+    v->__len = 0;
+    v->__cap = 0;
+}
+
+void vec_drop_inner_each(Vec* v, mapFn drop) {
+    if (v->__data == NULL)
+        return;
+    vec_iter_ref(v, drop);
+    free(v->__data);
+    v->__len = 0;
+    v->__cap = 0;
 }
 
--- cutils/utils.h
+++ cutils/utils.h
@@ -10,7 +10,7 @@
  */
 typedef struct {
     char* __data;
-    size_t len, cap;
+    size_t __len, __cap;
 } String;
 
 
@@ -18,8 +18,8 @@ typedef struct {
  * Borrowed slice of a string
  */
 typedef struct {
-    char* __data;
-    size_t __start, len;
+    const char* __data;
+    size_t __len;
 } Str;
 
 
@@ -28,7 +28,7 @@ typedef struct {
  */
 typedef struct {
     void* __data;
-    size_t __item_size, len, cap;
+    size_t __item_size, __len, __cap;
 } Vec;
 
 
@@ -37,8 +37,16 @@ typedef struct {
  */
 typedef struct {
     void* __data;
-    size_t __item_size, len;
+    size_t __item_size, __len;
 } Slice;
+
+
+/* Function used to modify elements in a container
+ * Used by containers, like `Vec`, as a "drop function" to allow
+ * cleaning up elements, which may be or contain owned pointers
+ * to other memory, before dropping the container's backing data.
+ */
+typedef void (*mapFn)(void*);
 
 
 /* -------------------------- */
@@ -51,12 +59,18 @@ String string_new();
 String string_with_capacity(size_t cap);
 
 /* Construct a new String, copying the contents from the given `char*` */
-String string_copy_from_cstr(char* cstr);
+String string_copy_from_cstr(const char* cstr);
 
 /* Reads contents of a file into a string,
  * returning ownership of the data.
  */
 String read_file(const char* path);
+
+/* Return current `String` length */
+size_t string_len(String* s);
+
+/* Return current `String` capacity */
+size_t string_cap(String* s);
 
 /* Resize the given String with the new size */
 void string_resize(String* s, size_t new_cap);
@@ -70,10 +84,10 @@ void string_push_str(String* s, Str* str);
 /* Push as most `str_len` bytes copied from a `char*` onto the end of the String, resizing if necessary.
  * `str_len` bytes is expected to be the size of the cstr not including the trailing nul byte.
  */
-void string_push_cstr_bound(String* s, char* cstr, size_t str_len);
+void string_push_cstr_bound(String* s, const char* cstr, size_t str_len);
 
 /* Same as `string_push_cstr_bound` except all of the char* up to the nul byte will be pushed */
-void string_push_cstr(String* s, char* cstr);
+void string_push_cstr(String* s, const char* cstr);
 
 /* Index into a String */
 char string_index(String* s, size_t ind);
@@ -87,8 +101,20 @@ Str string_as_str(String* s);
 /* Trim surrounding whitespace from a String, retuning a borrowed Str view */
 Str string_trim_whitespace(String* s);
 
+/* Split String by line breaks into a `Vec` of `Str*` */
+Vec string_split_lines(String* s);
+
+/* Split String by whitespace into a `Vec` of `Str*` */
+Vec string_split_whitespace(String* s);
+
 /* Convert a String to a `char*` */
 char* string_as_cstr(String* s);
+
+/* Zero out the contents of the current string and set
+ * the length of the `String` to zero. This will not affect the current
+ * capacity of the `String`. Subsequent writes will overwrite any existing data.
+ */
+void string_clear(String* s);
 
 /* free the inner data held by a String */
 void string_drop_inner(void* string_ptr);
@@ -98,13 +124,27 @@ void string_drop_inner(void* string_ptr);
 /* ----- Str functions ------ */
 /* -------------------------- */
 /* Construct a new Str from a char* without copying any data */
-Str str_from_cstr(char* cstr);
+Str str_from_cstr(const char* cstr);
+
+/* Construct a new Str from a char* and length without copying any data.
+ * Allows constructing a `Str` from an arbitrary slice of chars.
+ */
+Str str_from_ptr(const char* cstr, size_t len);
+
+/* Return current `Str` length */
+size_t str_len(Str* s);
 
 /* Trim surrounding whitespace returning another borrowed Str */
 Str str_trim_whitespace(Str* s);
 
+/* Split Str by line breaks into a `Vec` of `Str*` */
+Vec str_split_lines(Str* s);
+
 /* Split str by whitespace into a `Vec` of `Str*` */
-void str_split_whitespace(Vec* v, Str* s);
+Vec str_split_whitespace(Str* s);
+
+/* Return a pointer to the inner `Str` data */
+const char* str_as_ptr(Str* s);
 
 /* Convert to an owned String, copying internal data to the new String */
 String str_to_owned_string(Str* str);
@@ -113,7 +153,7 @@ String str_to_owned_string(Str* str);
 char str_index(Str* str, size_t ind);
 
 /* Return a pointer into a Str at the given index*/
-char* str_index_ref(Str* s, size_t ind);
+const char* str_index_ref(Str* s, size_t ind);
 
 
 /* -------------------------- */
@@ -124,6 +164,12 @@ Vec vec_new(size_t item_size);
 
 /* Construct a new empty `Vec` with the given capacity */
 Vec vec_with_capacity(size_t item_size, size_t cap);
+
+/* Return current `Vec` length */
+size_t vec_len(Vec* v);
+
+/* Return current `Vec` capacity */
+size_t vec_cap(Vec* v);
 
 /* Resize the given `Vec` with the new capacity */
 void vec_resize(Vec* v, size_t new_cap);
@@ -144,19 +190,31 @@ void vec_push(Vec* v, void* obj);
  * Vec v = vec_new(sizeof(Str*));
  * vec_push(&v, &str);
  * Str* str_ref = *(Str**)vec_index_ref(&v, 0);
+ * assert(str_ref == str);
  * ```
  */
 void* vec_index_ref(Vec* v, size_t ind);
 
-/* Free the inner data held by a `Vec` */
-void vec_drop_inner(void* vec_ptr);
+/* Apply the `func` function to each element in the `Vec`.
+ */
+void vec_iter_ref(Vec* v, mapFn func);
 
-typedef void (*dropFn)(void*);
+/* Apply the `drop` function to each element and then set
+ * the length of the `Vec` to zero. This will not affect the current
+ * capacity of the `Vec`. Subsequent writes will overwrite any existing data.
+ */
+void vec_clear(Vec* v, mapFn drop);
 
 /* Free the inner data held by a `Vec` after applying
  * the given `drop` function to each element
  */
-void vec_drop_inner_each(Vec* v, dropFn drop);
+void vec_drop_inner_each(Vec* v, mapFn drop);
+
+/* Free the inner data held by a `Vec`
+ * Use `vec_drop_inner_each` if the elements need to be cleaned
+ * up before being cleared out, e.g. a vec of pointers.
+ */
+void vec_drop_inner(void* vec_ptr);
 
 
 #endif
