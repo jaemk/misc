72504d5e9442de4daaacf3eab30e4bdd2749ef92
James Kominick <james@kominick.com>
2020-12-05 14:42:17 -0500

    2020/d05

--- adventofcode2020/rust/src/d05/mod.rs
+++ adventofcode2020/rust/src/d05/mod.rs
@@ -1,49 +1,73 @@
 use crate::utils::err;
 use crate::utils::file;
-use itertools::Itertools;
 
-fn parse(input: &str) -> err::Result<Vec<&str>> {
-    Ok(input.lines().collect::<Vec<_>>())
+struct Seat<'a> {
+    #[allow(unused)]
+    code: &'a str,
+    row: usize,
+    col: usize,
+}
+impl<'a> Seat<'a> {
+    fn id(&self) -> usize {
+        Self::get_id(self.row, self.col)
+    }
+    fn get_id(row: usize, col: usize) -> usize {
+        (row * 8) + col
+    }
 }
 
-fn part1(input: &[&str]) -> err::Result<u64> {
+#[inline]
+fn reduce_code(code: &str, zero: &str, one: &str) -> usize {
+    let code = code.replace(zero, "0").replace(one, "1");
+    usize::from_str_radix(&code, 2).expect("invaid code")
+}
+
+fn parse(input: &str) -> err::Result<Vec<Seat>> {
+    Ok(input
+        .lines()
+        .map(|code| {
+            let fbs = &code[..7];
+            let row = reduce_code(fbs, "F", "B");
+            let lrs = &code[7..];
+            let col = reduce_code(lrs, "L", "R");
+
+            let s = Seat { code, row, col };
+            Ok(s)
+        })
+        .collect::<err::Result<Vec<_>>>()?)
+}
+
+fn part1(input: &[Seat]) -> err::Result<usize> {
     Ok(input
         .iter()
-        .map(|line| {
-            let fbs = &line[..7];
-            let lrs = &line[7..];
-
-            let mut row_min = 0;
-            let mut row_max = 127;
-            for b in fbs.as_bytes() {
-                let mut change = (row_max - row_min) / 2;
-                if *b == b'F' {
-                    row_max -= change + 1;
-                } else {
-                    row_min += change + 1;
-                }
-            }
-            let row = row_min;
-
-            let mut row_min = 0;
-            let mut row_max = 7;
-            for b in lrs.as_bytes() {
-                let mut change = (row_max - row_min) / 2;
-                if *b == b'L' {
-                    row_max -= change + 1;
-                } else {
-                    row_min += change + 1;
-                }
-            }
-            let col = row_min;
-            (row * 8) + col
-        })
+        .map(|seat| seat.id())
         .max()
-        .expect("no max..."))
+        .ok_or("no max...")?)
 }
 
-fn part2(input: &[&str]) -> err::Result<u64> {
-    Ok(1)
+fn part2(input: &[Seat]) -> err::Result<usize> {
+    let mut plane = (0..128)
+        .map(|_| {
+            let mut row = set!(size = 8);
+            (0..8).for_each(|col| {
+                row.insert(col);
+            });
+            row
+        })
+        .collect::<Vec<_>>();
+
+    for seat in input {
+        let row = plane.get_mut(seat.row).ok_or("must be there")?;
+        row.remove(&seat.col);
+    }
+
+    for (row_id, row) in plane.iter().enumerate().skip(1) {
+        if row.len() == 1 {
+            let col = *row.iter().next().unwrap();
+            return Ok(Seat::get_id(row_id, col));
+        }
+    }
+    Err("failed to find seat".into())
 }
 
 pub fn run() -> err::Result<()> {
@@ -67,22 +91,26 @@ pub fn run() -> err::Result<()> {
 #[cfg(test)]
 mod test {
     use super::*;
-    static INPUT: &[(&str, u32, u32, u64)] = &[
+    use itertools::Itertools;
+
+    static INPUT: &[(&str, usize, usize, usize)] = &[
         ("FBFBBFFRLR", 44, 5, 357),
         ("BFFFBBFRRR", 70, 7, 567),
         ("FFFBBBFRRR", 14, 7, 119),
         ("BBFFBBFRLL", 102, 4, 820),
     ];
+
     #[test]
     fn test_p1() {
         let input = INPUT.iter().map(|(s, _, _, _)| s).join("\n");
         let input = parse(&input).expect("parse fail");
+        for (seat, (code, row, col, id)) in input.iter().zip(INPUT) {
+            assert_eq!(&seat.code, code);
+            assert_eq!(&seat.row, row);
+            assert_eq!(&seat.col, col);
+            assert_eq!(&seat.id(), id);
+        }
+
         assert_eq!(part1(&input).expect("p1 fail"), 820);
     }
-
-    // #[test]
-    // fn test_p2() {
-    //     let input = parse(INPUT).expect("parse fail");
-    //     assert_eq!(part2(&input).expect("p2 fail"), 336);
-    // }
 }
