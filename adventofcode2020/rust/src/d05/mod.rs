ef075a396381e8d397af0871e8898a68ff04f42a
James Kominick <james@kominick.com>
2020-12-05 15:25:45 -0500

    2020/d05 simplify

--- adventofcode2020/rust/src/d05/mod.rs
+++ adventofcode2020/rust/src/d05/mod.rs
@@ -4,16 +4,18 @@ use crate::utils::file;
 struct Seat<'a> {
     #[allow(unused)]
     code: &'a str,
+    #[allow(unused)]
     row: usize,
+    #[allow(unused)]
     col: usize,
+    id: usize,
 }
-impl<'a> Seat<'a> {
-    fn id(&self) -> usize {
-        Self::get_id(self.row, self.col)
-    }
-    fn get_id(row: usize, col: usize) -> usize {
-        (row * 8) + col
-    }
+
+#[inline]
+fn make_id(row: usize, col: usize) -> usize {
+    // id's are a sequence of ints since we're multiplying by
+    // 8 and col is in range 0..8
+    (row * 8) + col
 }
 
 #[inline]
@@ -23,48 +25,35 @@ fn reduce_code(code: &str, zero: &str, one: &str) -> usize {
 }
 
 fn parse(input: &str) -> err::Result<Vec<Seat>> {
-    Ok(input
+    let mut seats = input
         .lines()
         .map(|code| {
             let fbs = &code[..7];
             let row = reduce_code(fbs, "F", "B");
             let lrs = &code[7..];
             let col = reduce_code(lrs, "L", "R");
+            let id = make_id(row, col);
 
-            let s = Seat { code, row, col };
+            let s = Seat { code, row, col, id };
             Ok(s)
         })
-        .collect::<err::Result<Vec<_>>>()?)
+        .collect::<err::Result<Vec<_>>>()?;
+    seats.sort_unstable_by_key(|s| s.id);
+    Ok(seats)
 }
 
 fn part1(input: &[Seat]) -> err::Result<usize> {
-    Ok(input
-        .iter()
-        .map(|seat| seat.id())
-        .max()
-        .ok_or("no max...")?)
+    Ok(input[input.len() - 1].id)
 }
 
-fn part2(input: &[Seat]) -> err::Result<usize> {
-    let mut plane = (0..128)
-        .map(|_| {
-            let mut row = set!(size = 8);
-            (0..8).for_each(|col| {
-                row.insert(col);
-            });
-            row
-        })
-        .collect::<Vec<_>>();
-
-    for seat in input {
-        let row = plane.get_mut(seat.row).ok_or("must be there")?;
-        row.remove(&seat.col);
-    }
-
-    for (row_id, row) in plane.iter().enumerate().skip(1) {
-        if row.len() == 1 {
-            let col = *row.iter().next().unwrap();
-            return Ok(Seat::get_id(row_id, col));
+fn part2(seats: &[Seat]) -> err::Result<usize> {
+    let first_seat_id = seats[0].id;
+    let last_seat_id = seats[seats.len() - 1].id;
+    for seat_id in first_seat_id..last_seat_id + 1 {
+        let seat_index = seat_id - first_seat_id;
+        if seats[seat_index].id != seat_id {
+            // it's free
+            return Ok(seat_id);
         }
     }
     Err("failed to find seat".into())
@@ -102,15 +91,18 @@ mod test {
 
     #[test]
     fn test_p1() {
-        let input = INPUT.iter().map(|(s, _, _, _)| s).join("\n");
-        let input = parse(&input).expect("parse fail");
-        for (seat, (code, row, col, id)) in input.iter().zip(INPUT) {
-            assert_eq!(&seat.code, code);
-            assert_eq!(&seat.row, row);
-            assert_eq!(&seat.col, col);
-            assert_eq!(&seat.id(), id);
+        let input = INPUT.iter().map(|&(s, _, _, _)| s).join("\n");
+        let seats = parse(&input).expect("parse fail");
+
+        let mut sorted_input = INPUT.to_vec();
+        sorted_input.sort_unstable_by_key(|&(_, _, _, id)| id);
+        for (seat, (code, row, col, id)) in seats.iter().zip(sorted_input) {
+            assert_eq!(seat.code, code);
+            assert_eq!(seat.row, row);
+            assert_eq!(seat.col, col);
+            assert_eq!(seat.id, id);
         }
 
-        assert_eq!(part1(&input).expect("p1 fail"), 820);
+        assert_eq!(part1(&seats).expect("p1 fail"), 820);
     }
 }
