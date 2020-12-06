688f50c0f48066825096f6ea912a0b994afaae15
James Kominick <james@kominick.com>
2020-12-06 11:55:35 -0500

    add input download logic so I can do this on windows

--- adventofcode2020/rust/src/main.rs
+++ adventofcode2020/rust/src/main.rs
@@ -7,20 +7,56 @@ mod d03;
 mod d04;
 mod d05;
 
-macro_rules! report {
+#[inline]
+fn ensure_input(day: &str) -> utils::err::Result<bool> {
+    let input_dir = std::env::current_dir()?.join("..").join("input");
+    if !input_dir.exists() {
+        return Err("input dir is missing".into());
+    }
+    let input_file_name = format!("d{:0>2}.txt", day);
+    let input_file = input_dir.join(input_file_name);
+    if input_file.exists() && input_file.is_file() {
+        return Ok(true);
+    }
+
+    println!("  -> retrieving input for day {}", day);
+    let session_cookie = std::env::var("SESSION_COOKIE")
+        .map_err(|e| format!("missing SESSION_COOKIE env var: {}", e))?;
+    // url=https://adventofcode.com/2020/day/$day/input
+    let resp = ureq::get(&format!("https://adventofcode.com/2020/day/{}/input", day))
+        .set("cookie", &format!("session={}", session_cookie))
+        .call();
+    if let Some(e) = resp.synthetic_error() {
+        return Err(format!("request error: {:?}", e).into());
+    }
+    let mut new_file = std::fs::File::create(input_file)?;
+    std::io::copy(&mut resp.into_reader(), &mut new_file)?;
+
+    Ok(false)
+}
+
+macro_rules! day {
     ($day:expr, $body:expr) => {{
         println!("Day {}:", $day);
+        time!(ensure_input($day)?,
+        (res, ms) ->  {
+            if res {
+                println!("  -> input[{}ms] found existing file", ms);
+            } else {
+                println!("  -> input[{}ms] retrieved new file", ms);
+            }
+        });
         let (millis, _) = time!($body);
         println!("time: {}ms\n", millis);
     }};
 }
 
 fn run() -> utils::err::Result<()> {
-    report!("1", d01::run()?);
-    report!("2", d02::run()?);
-    report!("3", d03::run()?);
-    report!("4", d04::run()?);
-    report!("5", d05::run()?);
+    day!("1", d01::run()?);
+    day!("2", d02::run()?);
+    day!("3", d03::run()?);
+    day!("4", d04::run()?);
+    day!("5", d05::run()?);
     Ok(())
 }
 
