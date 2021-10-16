7b20cc0284cd47d6f2c18631859d1d4a85126fc8
James Kominick <james@kominick.com>
2021-10-16 00:03:51 -0400

    d04 more part-1 tests

--- adventofcode2019/lisp/tests/test-d04.lisp
+++ adventofcode2019/lisp/tests/test-d04.lisp
@@ -3,6 +3,15 @@
 
 (def-suite* test-d04 :in all)
 
+(test test-day04-part1-01
+  (is (advent19.d04:valid-a? 111111)))
+
+(test test-day04-part1-02
+  (is (not (advent19.d04:valid-a? 223450))))
+
+(test test-day04-part1-03
+  (is (not (advent19.d04:valid-a? 123789))))
+
 (test test-day04-part1-real
   (is (= 579
     (advent19.d04:part-1
