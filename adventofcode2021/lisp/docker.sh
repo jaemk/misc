ed9d4b8b15f92dc51474c40de6cab0a14329e6cb
James Kominick <james@kominick.com>
2021-12-01 12:06:01 -0500

    fix docker

--- adventofcode2021/lisp/docker.sh
+++ adventofcode2021/lisp/docker.sh
@@ -7,7 +7,7 @@ version="$(git rev-parse HEAD | awk '{ printf "%s", substr($0, 0, 7) }')"
 
 # options
 reg="${REGISTRY:-docker.jaemk.me}"
-app="advent19"
+app="advent"
 port_map_port="${PORT_MAP_PORT:-3003}"
 port_map="${PORT_MAP:-127.0.0.1:$port_map_port:$port_map_port}"
 
@@ -20,10 +20,12 @@ if [ -z "$cmd" ]; then
     echo "missing command..."
     exit 1
 elif [ "$cmd" = "build" ]; then
+    cp -r ../input input
     if [ ! -z "$version" ]; then
         docker build -t $reg/$app:$version .
     fi
     docker build -t $reg/$app:latest .
+    rm -r input/
 elif [ "$cmd" = "push" ]; then
     $0 build
     docker push $reg/$app:$version
